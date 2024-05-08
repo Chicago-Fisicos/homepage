64a115b401ed8d24cdd0507dac807cba1adf4f94
Santiago <saantivaazquez@gmail.com>
2024-05-08 19:36:08 -0300

    Muestro archivo csv con las coordenadas x,y.

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.write("Chicago Fisicos")
+
+url = ("https://raw.githubusercontent.com/95sv/repo_fisica/main/chewer.csv")
+
+df = pd.read_csv(url, delimiter=',')
+
+st.dataframe(df)
