922b5e1d9e7b4acc2e838900ebdcb04aa91b4285
Santivaz <saantivaazquez@gmail.com>
2024-05-24 16:53:55 -0300

    Agregado el video para la reproduccion del chipi

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import requests
 
 st.title("Chicago Fisicos")
 
@@ -8,3 +9,23 @@ url = ("https://raw.githubusercontent.com/95sv/repo_fisica/main/chewer.csv")
 df = pd.read_csv(url, delimiter=',')
 
 st.dataframe(df)
+
+#https://drive.google.com/file/d/1RWtqI_ZqExIyvrCIzwQQmMt0qDv67mHY/view?usp=sharing
+
+#https://drive.google.com/uc?export=download&id=1RWtqI_ZqExIyvrCIzwQQmMt0qDv67mHY
+
+# ID del video en Google Drive
+video_id = '1yRrGMfC26jI2qizgLljeAp7saDFM4Atu'
+
+# Enlace embebido de Google Drive
+video_url = 'https://drive.google.com/file/d/1yRrGMfC26jI2qizgLljeAp7saDFM4Atu/preview'
+
+# Título de la aplicación
+st.title("Video procesado chipi")
+
+# Insertar el video usando HTML
+st.markdown(f"""
+    <div style="text-align: center;">
+        <iframe src="{video_url}" width="640" height="480" allow="autoplay"></iframe>
+    </div>
+    """, unsafe_allow_html=True)
