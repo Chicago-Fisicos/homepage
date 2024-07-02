22e4a2f2880c2316195ee7d9f103956bafa2c1e5
Santivaz <saantivaazquez@gmail.com>
2024-07-02 17:41:28 -0300

    Agregando detalles de la correcion al informe

--- Pages/sonido.py
+++ Pages/sonido.py
@@ -7,6 +7,16 @@ import requests
 def app():
     st.markdown("# **AUDIO**")
 
+    texto="""
+    En este experimento, medimos la masa de la pelota de golf, la altura desde la cual se 
+    dejó caer la pelota, y utilizamos el audio del video para obtener los tiempos en los
+    que la pelota choca con el suelo de cemento.\n Estos datos nos permitieron calcular el
+    coeficiente de restitución, que mide el grado de conservación de la energía cinética.\n
+    Los resultados obtenidos confirman que en cada choque la pelota pierde altura debido
+    a la pérdida de energía, lo cual se refleja en la disminución de la velocidad
+    antes y después del choque.\n
+    """
+    st.markdown(texto)
     #LINK AUDIO:
     #   https://drive.google.com/file/d/1CczDtTo53xOFARmVvCf5P49mQDQipNFF/view?usp=drive_link
     #LINK VIDEO:
@@ -32,6 +42,39 @@ def app():
     # Mostrar el reproductor de audio en Streamlit
     st.audio(url2)
 
+    texto2= """
+        En un choque elástico, no se pierde energía. En estos choques, la energía cinética total se conserva.
+        El coeficiente de restitución está directamente relacionado con el estudio de choques, 
+        ya que proporciona una medida cuantitativa de cómo se comportan los cuerpos antes y 
+        después del choque.\n
+        Al calcular el coeficiente de restitución, podemos determinar cuánta energía cinética se conserva y cuánta se disipa en formas no mecánicas, como el calor o la deformación.
+        Esto nos permite clasificar el choque según su elasticidad.\n
+    """
+    st.markdown(texto2)
+
+    st.markdown("## Materiales y métodos")
+
+    texto3="""
+    Para este experimento, grabamos un video en un suelo de cemento.
+    Utilizamos un celular, un micrófono, una pelota de golf y una referencia 
+    métrica desde la cual lanzamos la pelota para obtener la altura.\n
+    Tomamos las medidas correspondientes en el **Sistema Internacional (SI)** o **MKS:** **metro (m)*,
+    **kilogramo (kg)** y **segundo (s).**\n
+    Pesamos la pelota de golf con una balanza.\n 
+    La referencia métrica fue un cartón con dimensiones de 0.3 m x 0.3 m.\n
+    Desde una altura de 0.3 m sobre el suelo, lanzamos la pelota de golf y grabamos el video.
+    En cuanto al audio, en formato **.wav**, primero lo pasamos por un ecualizador para eliminar 
+    el ruido de fondo, dejando solo los rebotes de la pelota.\n
+    Utilizando Python y librerías externas: 
+    - “numpy” para transformar el audio en un arreglo de enteros. 
+    - scipy.signal.find_peaks para encontrar los “picos” de sonido en el arreglo
+    cuánto más alto era un número, mayor era el pico, que interpretamos como
+    el choque de la pelota con el suelo, guardándolo en un archivo formato .csv. \n
+    Con los datos obtenidos, calculamos el coeficiente de restitución para cada rebote y
+    luego promediamos esos valores para obtener el valor promedio.\n
+    A continuación, se muestra la tabla de sonido con los picos mencionados anteriormente:
+    """
+    st.markdown(texto3)
     # Grafico de sonido:
     # https://github.com/Chicago-Fisicos/proyecto-fisica/blob/main/src/audios/graficos/grafico_picos.png
 
